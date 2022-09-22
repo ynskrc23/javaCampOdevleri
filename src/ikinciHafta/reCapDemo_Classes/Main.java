6ed5dcde447b7e89ad015c4afd8b28e543130c07
Yunus Karaca <ynskrc82@gmail.com>
2022-09-22 23:21:38 +0300

    ikinici hafta ödevleri

---
+++ src/ikinciHafta/reCapDemo_Classes/Main.java
@@ -0,0 +1,9 @@
+package ikinciHafta.reCapDemo_Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int islem = dortIslem.Topla(14,27);
+        System.out.println(islem);
+    }
+}

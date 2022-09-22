55efdee52e1524cf2a51f069c92c3d36bfcbfdf5
Yunus Karaca <ynskrc82@gmail.com>
2022-09-22 23:01:05 +0300

    ikinici hafta ödevleri

---
+++ src/ikinciHafta/classes/Main.java
@@ -0,0 +1,12 @@
+package ikinciHafta.classes;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerManager customerManager = new CustomerManager();
+        CustomerManager customerManager2 = new CustomerManager();
+        customerManager = customerManager2;
+        customerManager.Add();
+        customerManager.Update();
+        customerManager.Remove();
+    }
+}

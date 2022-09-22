abaea13e54ca0aca1b84985d89c4fd41869ab7cb
Yunus Karaca <ynskrc82@gmail.com>
2022-09-22 23:14:17 +0300

    ikinici hafta ödevleri

--- src/ikinciHafta/classes/Main.java
+++ src/ikinciHafta/classes/Main.java
@@ -8,5 +8,20 @@ public class Main {
         customerManager.Add();
         customerManager.Update();
         customerManager.Remove();
+
+        //value type
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1 = 30;
+        System.out.println(sayi2);
+
+        //referans type
+        int[] sayilar1 = {1,2,3};
+        int[] sayilar2 = {4,5,6};
+        sayilar2 = sayilar1;
+        sayilar1[0] = 10;
+        System.out.println(sayilar2[0]);
+
     }
 }

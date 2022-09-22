2bf9210cc302947e75435725b76cfd663ae1b1f7
Yunus Karaca <ynskrc82@gmail.com>
2022-09-22 22:30:18 +0300

    ikinici hafta ödevleri

--- src/ikinciHafta/methods2/Main.java
+++ src/ikinciHafta/methods2/Main.java
@@ -7,7 +7,10 @@ public class Main {
         System.out.println(yeniMesaj);
 
         int sayi = topla(23,82);
-        System.out.println("toplam: "+sayi);
+        System.out.println(sayi);
+
+        int toplam = topla2(23,82,25,34,27,38);
+        System.out.println("toplam: "+toplam);
     }
 
     public static void ekle(){
@@ -24,6 +27,16 @@ public class Main {
 
     public static int topla(int sayi1, int sayi2){
         return sayi1 + sayi2;
+    }
+
+    public static int topla2(int... sayilar){
+        int toplam = 0;
+
+        for(int sayi:sayilar){
+            toplam += sayi;
+        }
+
+        return toplam;
     }
 
     public static String sehir(){

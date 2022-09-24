a73ba1adeec18d20151a4a889c314f4ce949b12a
Yunus Karaca <ynskrc82@gmail.com>
2022-09-24 13:27:30 +0300

    ikinici hafta ödevleri Field ve Attribute ile Çalışmak

---
+++ src/ikinciHafta/classesWithAttributes/ProductManager.java
@@ -0,0 +1,7 @@
+package ikinciHafta.classesWithAttributes;
+
+public class ProductManager {
+    public void Add(Product product){
+        System.out.println("ürün eklendi "+ product.name);
+    }
+}
